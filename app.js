deb5f5f48e39321c43a7957f424bfa68bed7920d
Sammer Aburumman <sahabur@iu.edu>
2021-06-24 15:14:13 -0400

    Made main app.js host all other apps

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const fs          = require('fs');
 const http        = require('http');
 const https       = require('https');
-const mailer      = require('nodemailer');
 const helmet      = require('helmet');
 const express     = require('express');
-const cors        = require('cors');
-const email       = require('./secrets/email');
 
 // Express Initialization
 const app = express();
@@ -13,7 +10,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname, { dotfiles: 'allow' }));
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
 
 // Certificate
 const privateKey  = fs.readFileSync('secrets/privkey.pem', 'utf-8');
@@ -26,50 +22,13 @@ const credentials = {
   ca:   ca
 }
 
-// Mailing
-let smtpConfig = {
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
-  auth: {
-    user: email.email,
-    pass: email.password
-  }
-}
 
-let transporter = mailer.createTransport(smtpConfig);
 
-transporter.verify((error, success) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email server running')
-  }
-});
+// Applications
+app.use('/', require('./apps/portfolio/app').app)
 
-app.get('/', (req, res) => {
-  console.log(req.socket.remoteAddress )
-  res.sendFile(__dirname + '/public/index.html');
-});
 
-app.post('/submitContactForm', (req, res) => {
-  const data = req.body;
-  const mail = {
-    from: '"sammer.xyz" temprsammer@gmail.com',
-    to: 'iamsammert@gmail.com',
-    subject: 'New Message',
-    html: `From: ${data.name} (${data.email}) <br/><br/> Message: ${data.message}`
-  }
 
-  transporter.sendMail(mail, (err, data) => {
-    if (err) {
-      console.log(error);
-      res.status(500).send('Failed to send message');
-    } else {
-      res.status(200).send('Message sent!');
-    }
-  });
-});
 
 const httpServer  = http.createServer(app);
 const httpsServer  = https.createServer(credentials, app);
---
+++ apps/portfolio/app.js
@@ -0,0 +1,56 @@
+const mailer      = require('nodemailer');
+const express     = require('express');
+const email       = require('../../secrets/email');
+
+// Express Initialization
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname, { dotfiles: 'allow' }));
+app.use(express.json());
+
+// Mailing
+let smtpConfig = {
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: email.email,
+    pass: email.password
+  }
+}
+
+let transporter = mailer.createTransport(smtpConfig);
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email server running')
+  }
+});
+
+app.get('/', (req, res) => {
+  console.log(req.socket.remoteAddress )
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/submitContactForm', (req, res) => {
+  const data = req.body;
+  const mail = {
+    from: '"sammer.xyz" temprsammer@gmail.com',
+    to: 'iamsammert@gmail.com',
+    subject: 'New Message',
+    html: `From: ${data.name} (${data.email}) <br/><br/> Message: ${data.message}`
+  }
+
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      console.log(error);
+      res.status(500).send('Failed to send message');
+    } else {
+      res.status(200).send('Message sent!');
+    }
+  });
+});
+
+module.exports.app = app;
